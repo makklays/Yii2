a7ddb4e31469e92951bccfa164d4fa5a5ba2d618
Alexander Kuziv <makklays@gmail.com>
2017-12-26 22:03:05 +0300

    Create main.php

---
+++ frontend/config/main.php
@@ -0,0 +1,79 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'sourceLanguage' => 'en',
+    'language' => 'en',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+            'baseUrl' => '',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'urlManager' => [
+            //'class'=>'yii\web\UrlManager',
+            'showScriptName' => true,
+            'enablePrettyUrl' => true,
+            //'scriptUrl'=>'/index.php',
+            //'baseUrl' => 'index.php',
+            'class' => 'codemix\localeurls\UrlManager',
+            'languages' => ['ru', 'en'],
+            'enableDefaultLanguageUrlCode' => true,
+            'rules' => [
+                '' => 'site/index',
+                //'post/index' => 'post/index',
+                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@frontend/messages',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'post' => 'post.php',
+                        'app/auth' => 'auth.php'
+                    ]
+                ],
+            ],
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+        ],
+        /*'assetManager' => [
+            'basePath' => '@webroot/assets',
+            'baseUrl' => '@web/assets'
+        ],*/
+    ],
+    'params' => $params,
+];
