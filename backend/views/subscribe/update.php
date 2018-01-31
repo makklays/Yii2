88f27202a676d8cce3556e1e62aa1d05892b211e
Alexander Kuziv <makklays@gmail.com>
2018-02-01 01:02:39 +0300

    Create update.php

---
+++ backend/views/subscribe/update.php
@@ -0,0 +1,20 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Subscribe */
+
+$this->title = 'Update Subscribe: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Subscribes', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="subscribe-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
