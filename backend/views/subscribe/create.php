e36936a04716441b8fb5a975fd42f1a68e280408
Alexander Kuziv <makklays@gmail.com>
2018-02-01 01:01:12 +0300

    Create create.php

---
+++ backend/views/subscribe/create.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Subscribe */
+
+$this->title = 'Create Subscribe';
+$this->params['breadcrumbs'][] = ['label' => 'Subscribes', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="subscribe-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
