d91f2f385649b3cce765f55c9db730712fe8a8af
Alexander Kuziv <makklays@gmail.com>
2018-02-01 00:56:14 +0300

    Create create.php

---
+++ backend/views/tag/create.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Tag */
+
+$this->title = 'Create Tag';
+$this->params['breadcrumbs'][] = ['label' => 'Tags', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tag-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'frequency')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Create', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
