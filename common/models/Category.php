60dffa84e2bee17485aa42924f7c6e8eff7f3b13
Alexander Kuziv <makklays@gmail.com>
2018-02-03 01:41:27 +0300

    Create Category.php

---
+++ common/models/Category.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property integer $is_active
+ * @property integer $added
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['is_active'], 'integer'],
+            [['added'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'is_active' => 'Is Active',
+            'added' => 'Date',
+        ];
+    }
+}
